650c91a5e21292474bf149913a61f7b35591e565
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-05-24 18:59:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
 
 function deleteFromDriverByKey(driver, key){
   const newDriver = Object.assign({}, driver);
-  delete newDriver[key];
+  delete newDriver[`${key}`];
 }
 
 function destructivelyDeleteFromDriverByKey(driver, key){
