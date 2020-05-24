b52e5b15713556403d702c1396ed497175557bc7
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-05-24 18:45:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 // Write your solution in this file!
 let driver = {};
+
+updateDriverWithKeyValuePaire(driver, key, value){
+  newDriver = Object.assign({}, driver);
+  newDriver[key] = value;
+  return newDriver;
+}
